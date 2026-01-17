dcc0f56a5330ad1e84a5ba9521a01cda631d7b6a
Mizael Douglas <mizaeldouglas2@gmail.com>
2026-01-17 16:51:22 -0300

    fix: adicionado MySQL e configurando EntityFramework

--- ApiCatalago/Program.cs
+++ ApiCatalago/Program.cs
@@ -1,3 +1,6 @@
+using ApiCatalago.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+const string defaultConnection = "DefaultConnection";
+
+string mySqlConnection = builder.Configuration.GetConnectionString(name: defaultConnection);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 
 var app = builder.Build();
 
